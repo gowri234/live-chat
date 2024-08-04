a8f2c694b4791147cdcae48d2db2ff6a75251719
gowri234 <154495878+gowri234@users.noreply.github.com>
2024-08-04 23:32:05 +0530

    Update ChatWindow.js

--- src/PossibleSolution/ChatWindow.js
+++ src/PossibleSolution/ChatWindow.js
@@ -5,16 +5,8 @@ import AddMessage from "./AddMessage";
 import MessageHistory from "./MessageHistory";
 import PropTypes from "prop-types";
 
-/*
-This component passes a callback function to AddMessage (called "onMessage")
-because we will need to add new messages to the message history in BOTH chat windows.
-Because BOTH chat windows need access to the messages piece of state, that piece of
-state will live in the App.js file. That means that the callback function
-will be passed from the App Component all the way down to the AddMessage Component.
-*/
 class ChatWindow extends Component {
   onMessage = (message) => {
-    //The callback is getting called.
     this.props.onMessage(this.props.user.username, message);
   };
 
