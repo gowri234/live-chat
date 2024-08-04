0293b8b687c3f9da4139accba690aeb7f8965b1c
gowri234 <gowridabbe410@gmail.com>
2024-08-04 22:57:46 +0530

    creator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+app.js; ////
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ChatDashboard from "./ChatDashboard";
+
+class App extends Component {
+  state = {
+    messages: [
+      { username: "gowri", text: "Hi, gowthami!" },
+      { username: "gowri", text: "How are you?" },
+      { username: "gowthami", text: "Hi, gowri! Good, you?" },
+    ],
+    users: [{ username: "Gowri" }, { username: "gowthami" }],
+  };
+
+  updateMessageListing = (message) => {
+    let update_messages_list = this.state.messages;
+    update_messages_list.push(message);
+
+    this.setState({
+      messages: update_messages_list,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div className="container">
+          {this.state.users.map((user) => (
+            <ChatDashboard
+              key={user.username}
+              user={user}
+              updateMessages={this.updateMessageListing}
+              messages={this.state.messages}
+            />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
